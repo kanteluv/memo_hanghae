5de7159407de261e48805bda45324819d4bd55a7
seongyun <kanteluv0417@gmail.com>
2023-04-21 12:08:36 +0900

    회원가입시 패턴 정규식 수정

--- src/main/java/com/sparta/hanghaemamo/dto/UserRequestDto.java
+++ src/main/java/com/sparta/hanghaemamo/dto/UserRequestDto.java
@@ -13,11 +13,15 @@ import javax.validation.constraints.Size;
 public class UserRequestDto {
 
     @Size(min = 4, max = 10, message = "id는 4 이상, 10 이하만 가능합니다.")
-    @Pattern(regexp =  "/[a-z0-9]/", message = "id는 소문자, 숫자만 가능합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "id는 소문자, 숫자만 가능합니다.")
     @NotNull(message = "id를 입력해주세요")
     private String username;
     @Size(min = 8, max = 15, message = "password는 8 이상, 15 이하만 가능합니다.")
-    @Pattern(regexp =  "/[a-zA-Z0-9]/", message = "password는 소문자, 숫자만 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z\\p{Punct}0-9]*$", message = "password는 알파벳 대소문자, 특수문자, 숫자만 가능합니다.")
+//    ^ : 문자열 시작 $ : 문자열 끝
+//    [a-zA-Z\\p{Punct}] : 알파벳 대소문자와 특수문자를 허용
+//    \\p{Punct}는 Unicode punctuation character를 나타내며, 이에는 대부분의 특수문자가 포함
+//    *로 이 문자열이 0개 이상인지 여부, 공백을 포함하지 않음
     @NotNull(message = "password를 입력해주세요")
     private String password;
 
